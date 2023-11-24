a507a868b177b27a90e00ab8b08f12a886763f00
jonnnyhdz <jonathanhdz262@gmail.com>
2023-11-23 22:56:08 -0500

    Árbol Binario, Jonathan Hernandez

--- ArbolesBinarios.py
+++ ArbolesBinarios.py
@@ -1,3 +1,6 @@
+#Clase NodoArbol representa un nodo individual en el Árbol. 
+#Cada nodo tiene un dato, un puntero a su hijo izquierdo y un puntero a su hijo derecho.
+
 class NodoArbol:
     def __init__(self, dato):
         self.dato = dato
@@ -5,16 +8,23 @@ class NodoArbol:
         self.derecha = None
 
 
+#Clase ArbolBinario representa el árbol en sí. 
+#Tiene un atributo llamado "raiz" que es el nodo principal del Árbol.
+
 class ArbolBinario: 
     def __init__(self):
         self.raiz =  None
 
+
+#Función insertar que permite agregar un nuevo número al Árbol. 
 
     def insertar (self, dato):
         if self.raiz is None:
             self.raiz = NodoArbol(dato)
         else:
             self._insertar_recursivo(self.raiz, dato)
+
+#Si el árbol está vacío, crea un nuevo nodo con el dato proporcionado y lo establece como la raíz, en el caso opuesto; llama a la función privada _insertar_recursivo.
 
     def _insertar_recursivo(self, nodo, nuevo_dato):
         if nuevo_dato == nodo.dato:
@@ -33,6 +43,8 @@ class ArbolBinario:
                 self._insertar_recursivo(nodo.derecha, nuevo_dato)
 
 
+#Esta función de mostrar_arbol imprime el Árbol en la consola y llama a la función privada _mostrar_arbol_recursivo.
+
     def mostrar_arbol(self):
         self._mostrar_arbol_recursivo(self.raiz,0)
     def _mostrar_arbol_recursivo(self, nodo, nivel):
@@ -41,6 +53,8 @@ class ArbolBinario:
             print ("    "*nivel + str(nodo.dato))
             self._mostrar_arbol_recursivo(nodo.izquierda, nivel +1)
 
+
+#Aquí la función de buscar hace la busqueda de un dato en el Árbol y devuelve el nodo que contiene ese dato.
 
     def buscar (self, dato):
         return self._buscar_recursivo(self.raiz, dato)
@@ -51,6 +65,8 @@ class ArbolBinario:
             return self._buscar_recursivo(nodo.izquierda, dato)
         return self._buscar_recursivo(nodo.derecha, dato)
 
+
+#Esta función elimina un dato del Árbol.
 
     def eliminar (self, dato):
         self.raiz = self._eliminar_recursivo(self.raiz, dato)
@@ -70,13 +86,15 @@ class ArbolBinario:
             nodo.derecha = self._eliminar_recursivo(nodo.derecha, nodo.dato)
         return nodo
 
-
     def _encontar_minimo(self, nodo):
         actual = nodo
         while actual.izquierda is not None:
             actual = actual.izquierda
         return actual.dato
-    
+
+
+#Función de recorrer en PreOrden devuelve una lista con los datos del Árbol en recorrido PreOrden.
+
     def recorrer_preOrden(self):
         resultado = []
         self.preOrden(self.raiz, resultado)
@@ -87,6 +105,9 @@ class ArbolBinario:
             self.preOrden(nodo.izquierda, resultado)
             self.preOrden(nodo.derecha, resultado)
 
+
+#Función de recorrer en InOrden devuelve una lista con los datos del Árbol en recorrido InOrden.
+
     def recorrer_inOrden(self):
         resultado = []
         self.inOrden(self.raiz, resultado)
@@ -96,6 +117,9 @@ class ArbolBinario:
             self.inOrden(nodo.izquierda, resultado)
             resultado.append(nodo.dato)
             self.inOrden(nodo.derecha, resultado)
+
+
+#Función de recorrer en PostOrden devuelve una lista con los datos del Árbol en recorrido PostOrden.
 
     def recorrer_postOrden(self):
         resultado = []
@@ -109,35 +133,36 @@ class ArbolBinario:
 
 
 
+#Código para probar la implementación del Árbol:
 
-# Llamando a que se ejecute la clase
+#Llamando a que se ejecute la clase
 arbol = ArbolBinario()
 
-# Insertar Elementos Predeterminadamente
+#Insertar Elementos Predeterminadamente
 elemento_para_insertar = [8, 3, 1, 10, 14, 6, 7, 4, 13]
 for elemento in elemento_para_insertar:
     arbol.insertar(elemento)
 
-# Mostrar Arbol Completo
+#Mostrar Arbol Completo
 arbol.mostrar_arbol()
 
-# Insertar elementos en la consola
+#Insertar elementos en la consola
 while True:
-    # Preguntar al usuario si desea ingresar números
+    #Preguntar al usuario si desea ingresar números
     respuesta = input("¿Desea ingresar números al Árbol? (si/no): ").lower()
 
     if respuesta != 'si':
-        break  # Salir del bucle si la respuesta no es "si"
+        break  #Salir del bucle si la respuesta no es "si"
 
-    # Solicitar la cadena de números al usuario y convertirla a una lista
+    #Solicitar la cadena de números al usuario y convertirla a una lista
     numeros_ingresar = input("Ingrese el (los) números que desea poner en el Árbol seguidos de un espacío: ")
     numeros_ingresar = [int(num) for num in numeros_ingresar.split()]
 
-    # Insertar cada número en el Árbol
+    #Insertar cada número en el Árbol
     for numero_ingresar in numeros_ingresar:
         arbol.insertar(numero_ingresar)
 
-# Mostrar el Árbol después de la inserción
+#Mostrar el Árbol después de la inserción
 arbol.mostrar_arbol()
 
 
@@ -164,7 +189,7 @@ else:
 arbol.mostrar_arbol()
 
 
-#RCORRIDOS
+#RECORRIDOS
 
 #El rrecorrido en PreOrden
 print("El rrecorrido en PreOrden es: ", arbol.recorrer_preOrden())
